80dd643f3b987957719524cbdf70023adf09b91b
Innaky <innaky@protonmail.com>
2019-07-25 20:09:03 -0400

    (nw fn) parsing arguments! :D This binary return a random string, random numbers or random string-numbers ./random_string -n for random numbers ./random_string -s for random strings ./random_string -m for random numbers-strings.

--- c-generics/random_string.c
+++ c-generics/random_string.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int
 main(int argc, char *argv[]) {
 
   int string_length;
+  int option;
+  int nflag = 0;
+  int sflag = 0;
+  int mflag = 0;
 
   srand(time(NULL));
   printf("Input the length of the string: ");
   scanf("%i", &string_length);
 
-  numstr(string_length);
-  letters(string_length);
-  numbers(string_length);
+  while((option = getopt (argc, argv, "nsm")) != -1)
+    switch (option) {
+    case 'n':
+      nflag = 1;
+      break;
+    case 's':
+      sflag = 1;
+      break;
+    case 'm':
+      mflag = 1;
+      break;
+    default:
+      usage();
+    }
+
+  if (nflag) {
+    numbers(string_length);
+  }
+
+  if (sflag) {
+    letters(string_length);
+  }
+
+  if (mflag) {
+    numstr(string_length);
+  }
 
   return 0;
+}
+
+void
+usage(void)
+{
+  fprintf(stderr, "usage: random [-nsm]\n");
+  exit(1);
 }
 
 void

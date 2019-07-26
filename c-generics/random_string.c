b2fcccc38c198a00d9fc0289f38803f26fe2ab33
Innaky <innaky@protonmail.com>
2019-07-25 20:51:31 -0400

    (nw fn) Capture and transforming the second element. usage: ./random -n 40 3021308141155671139602335578774208330344 ./random -s 40 ogWEpUyuqQvpBLqQFPuzWEnrcINOvJfKPDQHaobq ./random -m 40 l6RktD9A0pcJl4ZhUFsTvxKJ5K9291NkXvufWrDK

--- c-generics/random_string.c
+++ c-generics/random_string.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <unistd.h>
+#include <unistd.h> // for getopt
 
 int
 main(int argc, char *argv[]) {
 
-  int string_length;
   int option;
+  int option_number;
   int nflag = 0;
   int sflag = 0;
   int mflag = 0;
 
+  if (argc > 3)
+    usage();
+
+  option_number = atoi(argv[2]);
+
   srand(time(NULL));
-  printf("Input the length of the string: ");
-  scanf("%i", &string_length);
 
   while((option = getopt (argc, argv, "nsm")) != -1)
     switch (option) {
@@ -32,15 +35,15 @@ main(int argc, char *argv[]) {
     }
 
   if (nflag) {
-    numbers(string_length);
+    numbers(option_number);
   }
 
   if (sflag) {
-    letters(string_length);
+    letters(option_number);
   }
 
   if (mflag) {
-    numstr(string_length);
+    numstr(option_number);
   }
 
   return 0;
@@ -49,12 +52,14 @@ main(int argc, char *argv[]) {
 void
 usage(void)
 {
-  fprintf(stderr, "usage: random [-nsm]\n");
+  fprintf(stderr, "usage: random [-nsm] number.\n");
   exit(1);
 }
 
+
 void
-letters(int str_lng) {
+letters(int str_lng)
+{
   int rletter;
   char letters[] = {"abcdefghijklmnopqrstuvwzyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 
@@ -67,7 +72,8 @@ letters(int str_lng) {
 }
 
 void
-numbers(int num_lng) {
+numbers(int num_lng)
+{
   int rnum;
   char numbers[] = {"0123456789"};
 
@@ -82,7 +88,8 @@ numbers(int num_lng) {
 }
 
 void
-numstr(int mix_lng) {
+numstr(int mix_lng)
+{
   int rnumstr;
   char mix[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"};
 

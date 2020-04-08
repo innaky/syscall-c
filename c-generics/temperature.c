77f118174f0c1f0f44351ff394887db82c1de819
Innaky <innaky@protonmail.com>
2020-04-08 10:00:03 -0400

    Temperature calculus °C to °F °F to °C using small functions for calculate one value and table functions for calculate table values.

--- c-generics/temperature.c
+++ c-generics/temperature.c
@@ -1,23 +1,76 @@
 #include <stdio.h>
 
-#define JUMP 10
+float ftoc(float);
+float ctof(float);
+void tablec(float);
+void tablef(float);
 
-int main()
+float
+ftoc(float fahr)
 {
-  float cels, farh; 
-  int max;
+  float cels;
   
-  farh = 0;
-  
-  printf("Input the max value of the table: ");
-  scanf("%d", &max);
-  
-  while (farh <= max) {
-    cels = 5.0 * (farh - 32.0) / 9.0;
-    printf("%4.2f ºF is %4.2f ºC\n", farh, cels);
-    farh += JUMP;
+  cels = (fahr - 32) * 5.0/9.0;
+
+return cels;
+
+}
+
+float
+ctof(float cels)
+{
+  float fahr;
+
+  fahr = (cels * 9.0/5.0) + 32;
+
+  return fahr;
+}
+
+void
+tablec(float cels)
+{
+  int to = 0;
+  float toCelsius = 0;
+
+  while (to <= cels) {
+    if (to == 0) {
+      toCelsius = ftoc(to);
+    }
+    
+    printf("%6d °F to %6.2f °C\n", to, toCelsius);
+    to += 10;
+    toCelsius = ftoc(to);
   }
+}
+
+void
+tablef(float fahr)
+{
+  int to = 0;
+  float toFahrenheit = 0;
+
+  while (to <= fahr) {
+    if (to == 0) {
+      toFahrenheit = ctof(to);
+    }
+    
+    printf("%6d °C to %6.2f °F\n", to, toFahrenheit);
+    to += 10;
+    toFahrenheit = ctof(to);
+  }
+}
+
+int
+main()
+{
+  float input = 0;
+
+  printf("Input the temperature value\n");
+  scanf("%f", &input);
   
+  tablef(input);
+  printf("\n");
+  tablec(input);
+
   return 0;
-  
 }
